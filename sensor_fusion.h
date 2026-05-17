64c4b2bb32ff94d33be1c79cff503ed91881dc68
ZAKR <hazeem@zakr.life>
2026-05-17 10:05:56 +0530

    Update sensor_fusion.h

--- sensor_fusion.h
+++ sensor_fusion.h
@@ -1,1 +1,17 @@
+#ifndef SENSOR_FUSION_H
+#define SENSOR_FUSION_H
 
+#include <stdint.h>
+
+typedef struct
+{
+    float eeg[8];
+    float ppg;
+    float eda;
+    float imu[3];
+} sensor_frame_t;
+
+void sensor_fusion_init(void);
+void sensor_fusion_update(sensor_frame_t *frame);
+
+#endif
