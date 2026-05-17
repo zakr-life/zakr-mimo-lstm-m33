ee3dc038e98bf208bd96d775e32529f6e54615f4
ZAKR <hazeem@zakr.life>
2026-05-17 10:06:17 +0530

    Update sensor_fusion.c

--- sensor_fusion.c
+++ sensor_fusion.c
@@ -1,1 +1,15 @@
+#include "sensor_fusion.h"
+#include <stdlib.h>
 
+void sensor_fusion_init(void)
+{
+}
+
+void sensor_fusion_update(sensor_frame_t *frame)
+{
+    for (int i = 0; i < 8; i++)
+        frame->eeg[i] = (float)rand() / RAND_MAX;
+
+    frame->ppg = 0.5f;
+    frame->eda = 0.2f;
+}
