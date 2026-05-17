42224c1939b319aa09d1f6f01db8d14de35390fc
ZAKR <hazeem@zakr.life>
2026-05-17 10:00:50 +0530

    Refactor model to functional API and add TFLite export
    Refactored model definition to use functional API and added TFLite conversion.

--- export_model.py
+++ export_model.py
@@ -4,25 +4,16 @@ from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Bidirectional
 
-SEQ_LEN = 64
-FEATURES = 32
+inputs = tf.keras.Input(shape=(10, 60))
+x = tf.keras.layers.LSTM(64, return_sequences=True)(inputs)
+x = tf.keras.layers.LSTM(32)(x)
+outputs = tf.keras.layers.Dense(4, activation='softmax')(x)
 
-model = Sequential([
-    Bidirectional(
-        LSTM(64, return_sequences=True),
-        input_shape=(SEQ_LEN, FEATURES)
-    ),
-    LSTM(32),
-    Dense(16, activation='relu'),
-    Dense(4, activation='sigmoid')
-])
+model = tf.keras.Model(inputs, outputs)
+model.compile(optimizer='adam', loss='categorical_crossentropy')
 
-model.compile(
-    optimizer='adam',
-    loss='mse',
-    metrics=['mae']
-)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
 
-model.summary()
-
-model.save("mimo_lstm_fp32")
+with open('mimo_lstm_fp32.tflite', 'wb') as f:
+    f.write(tflite_model)
